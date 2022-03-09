fac5ebad67de751da87b8f07fc45642c232b1535
Koushlendra Kuamr <code.koushlendra@gmail.com>
2022-03-09 10:49:58 +0530

    Create content of the homepage

---
+++ src/home.js
@@ -0,0 +1,25 @@
+function openHome(){
+    const homeBody = document.querySelector('.main-body');
+    homeBody.classList.add('home-body');
+    homeBody.textContent = '';
+    homeBody.classList.remove('menu-body');
+    homeBody.classList.remove('credit-body');
+
+    const marketingSlogan = document.createElement('div');
+    const homeImage = document.createElement('div');
+    const imgElem = document.createElement('img');
+    const description = document.createElement('div');
+
+    marketingSlogan.classList.add('slogan');
+    homeImage.classList.add('image');
+    description.classList.add('description');
+
+    homeBody.appendChild(marketingSlogan);
+    homeBody.appendChild(homeImage);
+    homeBody.appendChild(description);
+    homeImage.appendChild(imgElem);
+
+    marketingSlogan.textContent = '"Life\'s too short for boring food."';
+    description.textContent = 'Good food is always cooking! Go ahead, order some yummy items from the menu.';
+}
+export default openHome;
